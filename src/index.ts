063e155aefddae7c9e1c128b119122ad2d4a1bcc
Igor Klepacki <neg4n@icloud.com>
2021-09-14 14:09:14 +0200

    Add generics for better TypeScript support

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,60 @@
 import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next'
 
-export type NextApiComposeMiddlewares = Array<(handler: NextApiHandler) => NextApiHandler>
+export type ExtendableNextApiRequest<T> = T extends NextApiRequest ? T : NextApiRequest
 
-export type NextApiComposeOptions = {
+export type ExtendedNextApiHandler<T, H = any> = (
+  request: ExtendableNextApiRequest<T>,
+  response: NextApiResponse<H>
+) => void | Promise<void>
+
+export type NextApiComposeMiddlewares<T> = Array<
+  (handler: ExtendedNextApiHandler<T>) => ExtendedNextApiHandler<T>
+>
+
+export type NextApiComposeOptions<T> = {
   sharedErrorHandler: (
     error: Error,
     request: NextApiRequest,
     response: NextApiResponse
   ) => void | Promise<void>
-  middlewareChain: NextApiComposeMiddlewares
+  middlewareChain: NextApiComposeMiddlewares<T>
 }
 
 /**
  * Higher order function that composes multiple middlewares into one API Handler.
  *
- * @param {NextApiComposeMiddlewares | NextApiComposeOptions} middlewareOrOptions Middlewares array **(order matters)** or options object with previously mentioned middlewares array as `chain` property and error handler shared by every middleware in the array as `sharedErrorHandler` property.
+ * @param {NextApiComposeMiddlewares | NextApiComposeOptions} middlewareOrOptions Middlewares array **(order matters)** or options object with previously mentioned middlewares array as `middlewareChain` property and error handler shared by every middleware in the array as `sharedErrorHandler` property.
  * @param {NextApiHandler} handler Next.js API handler
  * @returns Middleware composed with Next.js API handler
  */
-export function compose(
-  middlewareOrOptions: NextApiComposeMiddlewares | NextApiComposeOptions,
-  handler: NextApiHandler
+export function compose<T>(
+  middlewareOrOptions: NextApiComposeMiddlewares<T> | NextApiComposeOptions<T>,
+  handler: (
+    request: ExtendableNextApiRequest<T>,
+    response: NextApiResponse
+  ) => void | Promise<void>
 ) {
   const isOptions = !Array.isArray(middlewareOrOptions)
   const chain = isOptions ? middlewareOrOptions.middlewareChain : middlewareOrOptions
 
-  return async (request: NextApiRequest, response: NextApiResponse) => {
+  return async (request: ExtendableNextApiRequest<T>, response: NextApiResponse) => {
     if (chain.length === 0) {
       return handler(request, response)
     }
 
-    return chain.reduceRight((previousMiddleware, currentMiddleware) => {
-      return async (request, response) => {
-        try {
-          await currentMiddleware(previousMiddleware)(request, response)
-        } catch (error) {
-          if (isOptions && middlewareOrOptions.sharedErrorHandler) {
-            await middlewareOrOptions.sharedErrorHandler(error, request, response)
+    return chain.reduceRight<ExtendedNextApiHandler<T>>(
+      (previousMiddleware, currentMiddleware) => {
+        return async (request, response) => {
+          try {
+            await currentMiddleware(previousMiddleware)(request, response)
+          } catch (error) {
+            if (isOptions && middlewareOrOptions.sharedErrorHandler) {
+              await middlewareOrOptions.sharedErrorHandler(error, request, response)
+            }
           }
         }
-      }
-    }, handler)(request, response)
+      },
+      handler
+    )(request, response)
   }
 }
