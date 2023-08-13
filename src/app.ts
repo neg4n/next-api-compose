fd2d85bdeb7bd368bbceee46b56422dea4514758
Igor Klepacki <neg4n@icloud.com>
2023-08-14 00:58:23 +0200

    Add correct type inference in revamped compose and introduce tsup

--- src/app.ts
+++ src/app.ts
@@ -1,105 +1,80 @@
-import type { Promisable, RequireAtLeastOne } from 'type-fest'
-import { Any, List, O } from 'ts-toolbelt'
+import type { Promisable } from 'type-fest'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextResponse } from 'next/server'
+
+type ParamType<T> = T extends (...args: [infer P]) => any ? P : never
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never
 
 type NextApiRouteMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD' | 'OPTIONS'
 
-type NextApiRouteHandlerResponse = Promisable<NextApiResponse> | Promisable<Response>
+type NextApiMethodHandler = (
+  request: NextApiRequest
+) => Promisable<NextApiResponse> | Promisable<Response>
 
-type NextApiRouteMethodHandler = (request: NextApiRequest) => NextApiRouteHandlerResponse
-
-type NextApiRouteMiddleware<Intercepted> = (
-  request: Intercepted extends NextApiRequest ? Intercepted : NextApiRequest
-) => Promisable<void> | NextApiRouteHandlerResponse
-
-type ExtractMiddlewareInterceptedRequest<Type> = Type extends NextApiRouteMiddleware<
-  infer X
->[]
-  ? X[]
-  : Type extends NextApiRouteMiddleware<infer X>
-  ? X
-  : never
-
-export type ComposeParameters<
+type ComposeParameters<
   Methods extends NextApiRouteMethod,
-  InterceptedMiddleware extends NextApiRequest,
-  FinalRouteInterceptionResult 
+  MiddlewareChain extends Array<
+    (request: any) => Promisable<NextResponse> | Promisable<Response> | Promisable<void>
+  >
 > = Record<
   Methods,
-  | NextApiRouteMethodHandler
+  | NextApiMethodHandler
   | [
-      NextApiRouteMiddleware<InterceptedMiddleware>[],
-      (request: FinalRouteInterceptionResult) => NextApiRouteHandlerResponse
+      MiddlewareChain,
+      (request: UnionToIntersection<ParamType<MiddlewareChain[number]>>) => any
     ]
 >
 
-export async function compose<
+/**
+ * Function that allows to define complex API structure in Next.js App router's Route Handlers.
+ *
+ * @param {ComposeParameters} parameters Middlewares array **(order matters)** or options object with previously mentioned middlewares array as `middlewareChain` property and error handler shared by every middleware in the array as `sharedErrorHandler` property.
+ * @returns Method handlers with applied middleware.
+ */
+export function compose<
   UsedMethods extends NextApiRouteMethod,
-  InterceptedMiddleware extends NextApiRequest,
-  FinalRouteInterceptionResult = O.MergeAll<NextApiRequest, InterceptedMiddleware[]>
->(
-  parameters: ComposeParameters<
-    UsedMethods,
-    InterceptedMiddleware,
-    FinalRouteInterceptionResult extends NextApiRequest
-      ? FinalRouteInterceptionResult
-      : NextApiRequest
+  MiddlewareChain extends Array<
+    (request: any) => Promisable<NextResponse> | Promisable<Response> | Promisable<void>
   >
-) {
-  const modified = await Promise.all(
-    Object.entries(parameters).map(async ([rawMethod, rawComposeForMethodData]) => ({
-      [rawMethod as UsedMethods]: async (request: any) => {
-        const composeForMethodData = rawComposeForMethodData as
-          | NextApiRouteMethodHandler
-          | [
-              NextApiRouteMiddleware<InterceptedMiddleware>[],
-              (request: FinalRouteInterceptionResult) => NextApiRouteHandlerResponse
-            ]
-
-        if (Array.isArray(composeForMethodData)) {
-          const [middlewareChain, handler] = composeForMethodData
-
-          for (const middleware of middlewareChain) {
-            const abortedMiddleware = await middleware(request)
-
-            if (abortedMiddleware != null && abortedMiddleware instanceof Response)
-              return abortedMiddleware
-          }
-
-          return await handler(request as any)
+>(parameters: ComposeParameters<UsedMethods, MiddlewareChain>) {
+  const modified = Object.entries(parameters).map(
+    ([method, composeForMethodData]: [
+      UsedMethods,
+      (
+        | NextApiMethodHandler
+        | [
+            [(request: any) => any],
+            (request: UnionToIntersection<ParamType<MiddlewareChain>>) => any
+          ]
+      )
+    ]) => ({
+      [method]: async (request: any) => {
+        if (typeof composeForMethodData === 'function') {
+          const handler = composeForMethodData
+          return await handler(request)
         }
 
-        const singleHandler = composeForMethodData
-        await singleHandler(request)
+        const [middlewareChain, handler] = composeForMethodData
+
+        for (const middleware of middlewareChain) {
+          const abortedMiddleware = await middleware(request)
+
+          if (abortedMiddleware != null && abortedMiddleware instanceof Response)
+            return abortedMiddleware
+        }
+
+        return await handler(request)
       }
-    }))
+    })
   )
 
-  return modified.reduce<Record<UsedMethods, NextApiRouteMethodHandler>>(
+  return modified.reduce<Record<UsedMethods, any>>(
     (acc, obj) => ({ ...acc, ...obj }),
-    {} as Record<UsedMethods, NextApiRouteMethodHandler>
+    {} as Record<UsedMethods, any>
   )
 }
-
-async function m() {
-  const handlers = await compose({
-    GET: (request) => {
-      return new Response()
-    },
-    POST: [
-      [
-        (request: NextApiRequest & { foo: string }) => {
-          //@ts-ignore
-          request.foo = 'bar'
-          return new Response('siema')
-        }
-      ],
-      (request) => {
-        request.foo
-        console.log(request)
-        return new Response()
-      }
-    ]
-  })
-}
-m()
